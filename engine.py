62e742768a2467b93a24a255a83584a75e9b6913
Elias Kibet <ngetichn558@gmail.com>
2026-06-01 12:00:27 +0300

    Added requirements.txt for cloud deployment

--- engine.py
+++ engine.py
@@ -1,15 +1,15 @@
 import mysql.connector
 
 class SuperLenderEngine:
-    def __init__(self, host, database, user, password):
-        """Initializes the connection to the MySQL database engine."""
+    def __init__(self, host, database, user, password, port=3306):
         self.connection = mysql.connector.connect(
             host=host,
+            port=port,
             database=database,
             user=user,
-            password=password,
-            autocommit=True
+            password=password
         )
+        
         # dictionary=True allows us to read rows like loan['balance_remaining']
         self.cursor = self.connection.cursor(dictionary=True)
         print("Successfully connected to the MySQL database engine!")
--- server.py
+++ server.py
@@ -21,10 +21,11 @@ app.add_middleware(
 )
 
 db_engine = SuperLenderEngine(
-    host="localhost",
-    database="superlender_core",
-    user="root",
-    password="SuperLender2026!" 
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
 )
 
 # --- CREDENTIALS ---
